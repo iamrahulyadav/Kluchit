946472ba58b6d2d006c7f13895082dcea9738c84
Aaybee <abdullahmanzoordar@gmail.com>
2016-06-29 16:18:38 +0500

    changes by abdullah

--- app/src/main/java/com/cybussolutions/kluchit/Activities/FBregistration.java
+++ app/src/main/java/com/cybussolutions/kluchit/Activities/FBregistration.java
@@ -42,7 +42,7 @@ import java.util.Map;
 
 public class FBregistration extends AppCompatActivity {
 //rihana
-//sadjaskljdadfadas
+//sadjaskljdadfadasdsad
     Tracker t;
     private Analytics myApp;
     private Toolbar toolbar;
