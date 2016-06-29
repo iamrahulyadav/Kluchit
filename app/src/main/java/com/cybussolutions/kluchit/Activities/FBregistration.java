67d99d69038e54f222426ed6e7d9c61980226254
Aaybee <abdullahmanzoordar@gmail.com>
2016-06-29 16:22:24 +0500

    changes by abdullah1

--- app/src/main/java/com/cybussolutions/kluchit/Activities/FBregistration.java
+++ app/src/main/java/com/cybussolutions/kluchit/Activities/FBregistration.java
@@ -36,13 +36,18 @@ import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 
 import java.io.ByteArrayOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class FBregistration extends AppCompatActivity {
-//rihana
-//sadjaskljdadfadasdsadsaddsad
+
     Tracker t;
     private Analytics myApp;
     private Toolbar toolbar;
@@ -52,13 +57,13 @@ public class FBregistration extends AppCompatActivity {
 
     RadioGroup rgroup;
 
-    String user, pass, email, category, jobtype;
+    String user, pass, email, category, jobtype,user_na;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         final SharedPreferences pref = getApplicationContext().getSharedPreferences("MyfacebookPref", MODE_PRIVATE);
-        String fb_registraion = pref.getString("fb_registraion", null);
+        String fb_registraion = pref.getString("activity_fbregistration", null);
 
 
         if (fb_registraion != null) {
@@ -83,6 +88,7 @@ public class FBregistration extends AppCompatActivity {
         usercat = (EditText) findViewById(R.id.editText);
         userPassword = (EditText) findViewById(R.id.passwordfb);
         submit = (Button) findViewById(R.id.submit);
+
 
 
         userEmail.setFocusable(false);
@@ -127,7 +133,13 @@ public class FBregistration extends AppCompatActivity {
 
                 // getdata();
 
-                if (((EditText) findViewById(R.id.emialfb)).getText().toString().equals("") || ((EditText) (findViewById(R.id.passwordfb))).getText().toString().equals("") || ((EditText) findViewById(R.id.userid)).getText().toString().equals("") || ((EditText) findViewById(R.id.editText)).getText().toString().equals("") || ((EditText) findViewById(R.id.userid)).getText().toString().equals("") || (((RadioGroup) findViewById(R.id.radio_Group)).getCheckedRadioButtonId() != ((RadioButton) findViewById(R.id.in_house)).getId() && ((RadioGroup) findViewById(R.id.radio_Group)).getCheckedRadioButtonId() != ((RadioButton) findViewById(R.id.Out_House)).getId())) {
+                Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+                Matcher m = p.matcher(((EditText) findViewById(R.id.userid)).getText().toString());
+                boolean b = m.find();
+
+
+
+                if (b || ((EditText) findViewById(R.id.userid)).getText().toString().contains(" ") || ((EditText) findViewById(R.id.emialfb)).getText().toString().equals("") || ((EditText) (findViewById(R.id.passwordfb))).getText().toString().equals("") || ((EditText) findViewById(R.id.userid)).getText().toString().equals("") || ((EditText) findViewById(R.id.editText)).getText().toString().equals("")  || (((RadioGroup) findViewById(R.id.radio_Group)).getCheckedRadioButtonId() != ((RadioButton) findViewById(R.id.in_house)).getId() && ((RadioGroup) findViewById(R.id.radio_Group)).getCheckedRadioButtonId() != ((RadioButton) findViewById(R.id.Out_House)).getId())) {
                     if (((EditText) findViewById(R.id.emialfb)).getText().toString().equals("")) {
                         ((EditText) findViewById(R.id.emialfb)).setError("Please Write Your Email");
                     }
@@ -143,6 +155,10 @@ public class FBregistration extends AppCompatActivity {
                     }
                     if ((((RadioGroup) findViewById(R.id.radio_Group)).getCheckedRadioButtonId() != ((RadioButton) findViewById(R.id.in_house)).getId() && ((RadioGroup) findViewById(R.id.radio_Group)).getCheckedRadioButtonId() != ((RadioButton) findViewById(R.id.Out_House)).getId())) {
                         ((RadioButton) findViewById(R.id.in_house)).setError("Please select a category!");
+                    }
+                    if (b  || ((EditText) findViewById(R.id.userid)).getText().toString().contains(" "))
+                    {
+                        ((EditText) findViewById(R.id.userid)).setError("Username can not contain special characters");
                     }
                 } else {
 
@@ -188,6 +204,7 @@ public class FBregistration extends AppCompatActivity {
 
                                 //Adding parameters
                                 params.put("userImage", image);
+                                params.put("filename",userEmail.getText().toString());
 
                                 //returning parameters
                                 return params;
@@ -240,45 +257,77 @@ public class FBregistration extends AppCompatActivity {
                                 Map<String, String> params = new Hashtable<String, String>();
 
                                 //Adding parameters
-                                params.put("image", image);
+                                params.put("userImage", image);
+                                params.put("filename",userEmail.getText().toString());
+
 
                                 //returning parameters
                                 return params;
                             }
                         };
 
-
+                        // new comment
                         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
                         requestQueue.add(stringRequest);
                     }
 
 
+//holla
 
 
+                    String postuser=EndPoints.BASE_URL+"/common_controller/saveNewUserBySocial";
 
-                    /*StringRequest sr = new StringRequest(Request.Method.POST,"http://abdullahmanzoordar.net23.net//reg_k.php", new Response.Listener<String>() {
+                    StringRequest sr = new StringRequest(Request.Method.POST,postuser, new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
-                            if (!response.toString().contains("not"))
-                                Toast.makeText(getApplicationContext(),"Registered",Toast.LENGTH_LONG).show();
-                            else
-                                Toast.makeText(getApplicationContext(),"Not Registered",Toast.LENGTH_LONG).show();
+                            // if (!response.toString().contains("not"))
+                            Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+                            //else
+                            //  Toast.makeText(getApplicationContext(),"Not Registered",Toast.LENGTH_LONG).show();
                         }
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getApplicationContext(),"error in sending request",Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
                         }
                     }){
                         @Override
                         protected Map<String,String> getParams(){
                             Map<String,String> params = new HashMap<String, String>();
-                            String user_,pass_,employed_,categories_;
-                            user_=((EditText)findViewById(R.id.emialfb)).getText().toString();
+                            String user_e,pass_,categories_,username_;
+                            username_=((EditText)findViewById(R.id.userid)).getText().toString();
+                            user_e=((EditText)findViewById(R.id.emialfb)).getText().toString();
                             pass_=((EditText)findViewById(R.id.passwordfb)).getText().toString();
                             categories_=((EditText)findViewById(R.id.editText)).getText().toString();
-                            params.put("username",user_);
-                            params.put("password",pass_);
+                            params.put("username",username_.toLowerCase());//done
+                            params.put("password",pass_);//done
+                            params.put("is_active","1");//done
+                            params.put("email",user_e.toLowerCase());//done
+
+                            String timeStamp = (new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime())).toString();
+                            params.put("date_added",timeStamp);//done
+                            params.put("filename",user_e.toLowerCase()+".jpeg");//done
+
+
+                            String first="",last="";
+                            StringTokenizer st = new StringTokenizer(user_na);
+                            int i=0,count=0;
+                            while (st.hasMoreTokens()) {
+                                if (i==0)
+                                    first=st.nextToken();
+                                else {
+                                    if (count>0)
+                                        last+=" ";
+                                    last += st.nextToken();
+                                    count++;
+                                }
+                                i++;
+                            }
+
+                            params.put("first_name",first);//done
+                            params.put("last_name",last);//done
+
+
 
                             if (((RadioGroup) findViewById(R.id.radio_Group)).getCheckedRadioButtonId()==((RadioButton) findViewById(R.id.in_house)).getId()) {
                                 params.put("employed_from","in_house");
@@ -287,7 +336,7 @@ public class FBregistration extends AppCompatActivity {
                             {
                                 params.put("employed_from","out_house");
                             }
-                            params.put("categories",categories_);
+                            params.put("categories",categories_.toLowerCase());//done
 
                             return params;
                         }
@@ -300,7 +349,7 @@ public class FBregistration extends AppCompatActivity {
                         }
                     };
                     Volley.newRequestQueue(getApplicationContext()).add(sr);
-                    */
+
 
                 }
             }
@@ -317,7 +366,8 @@ public class FBregistration extends AppCompatActivity {
         else {
             ((ImageView) findViewById(R.id.imageView5)).setImageResource(R.drawable.applogo);
         }
-        ((EditText) findViewById(R.id.userid)).setText(intent.getStringExtra("name"));
+        user_na=intent.getStringExtra("name");
+        ((EditText) findViewById(R.id.userid)).setText(user_na);
     }
 
 
